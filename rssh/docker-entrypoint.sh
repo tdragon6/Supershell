2516b427e59a5f68fc93bb339c3bccc12971d2bc
tdragon6 <ljygz1998@163.com>
2023-03-28 17:45:23 +0000

    update Supershell

--- rssh/docker-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /app/bin
-exec /app/wait-for-it.sh -t 0 flask:5000 -- ./server --datadir /data --webserver :3232
--- rssh/pkg/logger/dummy.go
+++
@@ -1,8 +0,0 @@
-//go:build nologging
-// +build nologging
-
-package logger
-
-func (l *Logger) Ulogf(callerStackDepth int, u Urgency, format string, v ...interface{}) {
-
-}
